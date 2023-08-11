8f36e58c09274128ff00a4b624f8eb831b4c05cb
Farhan Noor Dehan <dehannoor3199@gmail.com>
2023-08-11 11:48:54 +0600

    eighteen

--- train_bert_gcn.py
+++ train_bert_gcn.py
@@ -296,9 +296,12 @@ def log_training_results(trainer):
     test_roc_auc = []
     num_classes = y_true_train.shape[1]
     for class_idx in range(num_classes):
-        y_pred_train_class = test_step(None, (idx,))[0][:, class_idx].cpu().numpy()
-        y_pred_val_class = test_step(None, (idx,))[0][:, class_idx].cpu().numpy()
-        y_pred_test_class = test_step(None, (idx,))[0][:, class_idx].cpu().numpy()
+        _, y_pred_train_class = test_step(None, (idx,))
+        _, y_pred_val_class = test_step(None, (idx,))
+        _, y_pred_test_class = test_step(None, (idx,))
+        y_pred_train_class = y_pred_train_class[0][:, class_idx].cpu().numpy()
+        y_pred_val_class = y_pred_val_class[0][:, class_idx].cpu().numpy()
+        y_pred_test_class = y_pred_test_class[0][:, class_idx].cpu().numpy()
     
         train_roc_auc.append(roc_auc_score(y_true_train[:, class_idx], y_pred_train_class))
         val_roc_auc.append(roc_auc_score(y_true_val[:, class_idx], y_pred_val_class))
