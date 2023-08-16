6e6ee953487a605bfb071de1aa2ff5de4cbd8445
Farhan Noor Dehan <dehannoor3199@gmail.com>
2023-08-16 17:36:08 +0600

    added banfake correction

--- data/py.py
+++ data/py.py
@@ -1,10 +1,20 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-train=pd.read_csv("/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/train.csv")
+# train=pd.read_csv("/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/train.csv")
 
-test=pd.read_csv("/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/test.csv")
+# test=pd.read_csv("/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/test.csv")
 # df=pd.read_csv('/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/finaldataset.csv')
 #df_val= pd.read_csv('Val.csv')
+auth=pd.read_csv('/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/LabeledAuthentic-7K.csv')
+fake=pd.read_csv('/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/LabeledFake-1K.csv')
+
+df = auth
+df = df.append(fake)
+df = df.sample(frac=1).reset_index(drop=True)
+from sklearn.model_selection import train_test_split
+
+train, test= train_test_split(df, test_size=0.2, random_state=121, stratify=df['label'])
+
 
 # list=[df_train,df_test,df_val]
 # list=[df_train,df_test]
@@ -28,13 +38,13 @@ test=pd.read_csv("/home/farhan/Documents/nlp/bertgcn-bangla/BERTGCN/data/test.cs
 # train,test = train_test_split(df, test_size=0.33, random_state=121,shuffle=True,stratify=df['tag'])
 
 # # print(df.head())
-df = pd.concat([train.assign(ind="train"),test.assign(ind="test")])
+df1 = pd.concat([train.assign(ind="train"),test.assign(ind="test")])
 
-df.reset_index(inplace=True)
+df1.reset_index(inplace=True)
 # print(df.head())
-df_corpus=df[['ind','label']]
+df_corpus=df1[['ind','label']]
 # df_corpus.reset_index()
-df_corpus.to_csv('BengaliHateSpeech(1).txt', sep='\t', index=True, header=False)
+df_corpus.to_csv('BanFake(1).txt', sep='\t', index=True, header=False)
 
 # df['Comments'].to_csv('SentNOB.txt', sep='\t')
 # df_corpus=df["Comments"]
@@ -43,9 +53,9 @@ df_corpus.to_csv('BengaliHateSpeech(1).txt', sep='\t', index=True, header=False)
 # df_corpus=df[['ind','Label']]
 # df['text'].to_csv('Emotion.txt', sep='\t')
 # df_corpus=df["Data"]
-df['text'].to_csv('BengaliHateSpeech.txt', sep='\t', index=False, header=False)
+df['content'].to_csv('BanFake.txt', sep='\t', index=False, header=False)
 
 
 from normalizer import normalize
-df['text'] = df['text'].apply(normalize)
-df['text'].to_csv('BengaliHateSpeech.clean.txt', sep='\t', index=False, header=False)
+df['content'] = df['content'].apply(normalize)
+df['content'].to_csv('BanFake.clean.txt', sep='\t', index=False, header=False)
